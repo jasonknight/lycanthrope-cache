23ba1576c8b3aef4f119970c8892569f0e71a70e
Jason Knight <jolierouge242@gmail.com>
2020-04-12 08:04:42 -0400

    Turning off logging

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 namespace Lycanthrope\Cache;
 function settings() {
     $o = new \stdClass;
-    $o->logging = 'on';
+    $o->logging = 'off';
     $o->log_file_path = __DIR__ . '/lycan.log';
     return $o;
 }
